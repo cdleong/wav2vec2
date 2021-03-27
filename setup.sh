18a2aeb933d0f148aff99bcc3703712779cf6802
colin.leong <colin.leong.1.ctr@us.af.mil>
2021-03-27 23:48:00 +0000

    CDL: setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pip install git+https://github.com/huggingface/datasets.git
+pip install torchaudio
+pip install librosa
+pip install jiwer
+pip install wandb 
+pip install git+https://github.com/huggingface/transformers.git
+export WANDB_ENTITY=wandb
+export WANDB_PROJECT=huggingface
+export WANDB_LOG_MODEL=true
+wandb login
