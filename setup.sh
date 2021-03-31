96fd832d3029add78924bd12fc64761633bb244f
Colin Leong <fake@email.com>
2021-03-31 19:01:49 +0000

    CDL: wrong WANDB Entity

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ pip install jiwer
 pip install wandb 
 pip install git+https://github.com/huggingface/transformers.git
 pip install ftfy
-export WANDB_ENTITY=wandb
+export WANDB_ENTITY=cleong1
 export WANDB_PROJECT=huggingface
 export WANDB_LOG_MODEL=true
 wandb login
